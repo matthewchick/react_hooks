cb0b7873a060b9cd91e32679d4654895e2677110
Matthew Chick <c.chick@unsw.edu.au>
2019-09-15 21:27:23 +1000

    Use component lifecycle when using class component

--- src/App.js
+++ src/App.js
@@ -1,44 +1,52 @@
 import React, { Component, useState } from 'react';  //use hooks
 
-const App = () => {
-  const [count, setCount] = useState(0);   //setCount is a function
+// const App = () => {
+//   const [count, setCount] = useState(0);   //setCount is a function
 
-  const increment = () => {
-    setCount(count + 1);
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   return (
+//     <div>
+//       <h2>counter app</h2>
+//       <button onClick={increment}>
+//         Clicked {count} times
+//       </button>
+//     </div>
+//   );
+
+// }
+
+class App extends Component {
+  state = {
+    count: 0
   };
 
-  return (
-    <div>
-      <h2>counter app</h2>
-      <button onClick={increment}>
-        Clicked {count} times
-      </button>
-    </div>
-  );
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
 
+  componentDidMount() {
+    document.title = `Clicked ${this.state.count} times`
+  }
+
+  componentDidUpdate() {
+    document.title = `Clicked ${this.state.count} times`
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>counter app</h2>
+        <button onClick={this.increment}>
+          Clicked {this.state.count} times
+        </button>
+      </div>
+    );
+  }
 }
-
-// class App extends Component {
-//   state = {
-//     count: 0
-//   };
-
-//   increment = () => {
-//     this.setState({
-//       count: this.state.count + 1
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <h2>counter app</h2>
-//         <button onClick={this.increment}>
-//           Clicked {this.state.count} times
-//         </button>
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
