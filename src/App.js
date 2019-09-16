bc1f65c77424fc34ea94f6a1901aa9ea49833b88
Matthew Chick <c.chick@unsw.edu.au>
2019-09-17 09:06:47 +1000

    Controlling userEffect's behaviour

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import React, { Component, useState, useEffect } from 'react';  //use hooks
 
 const App = () => {
-  //state
-  const [news, setNews] = useState([])
+  //state: useState is a Hook that lets you add React state to function components.
+  const [news, setNews] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('react');  //pass 'react' to searchQuery
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react');
+
   //fetch news
   const fetchNews = () => {
-    fetch('http://hn.algolia.com/api/v1/search?query=react')
-    .then(result => result.json())
-    .then(data => setNews(data.hits))
-    .catch(error => console.log(error));
+    fetch(url)
+      .then(result => result.json())
+      .then(data => setNews(data.hits))
+      .catch(error => console.log(error));
   }
 
-  useEffect(() => {
-    fetchNews()
-  })
+  useEffect(() => {   //we don't need to have component lifecycle as componentDidMount()
+    fetchNews();
+  }, [url]);
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`)
+  }
 
   return (
     <div>
       <h2>News</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={searchQuery} onChange={handleChange} />
+        <button>Search</button>
+      </form>
       {news.map((n, i) => (
         <p key={i}>{n.title}</p>
       ))}
@@ -25,7 +41,7 @@ const App = () => {
   )
 }
 // const App = () => {
-//   const [count, setCount] = useState(0);   //setCount is a function
+//   const [count, setCount] = useState(0);   //setCount is a function This is similar to this.state.count and this.setState in a class
 
 //   useEffect(() => {      //replace component lifecycle
 //     document.title = `Clicked ${count} times`;
